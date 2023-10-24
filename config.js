77ed1a07531882e4ab1c69e04b2ad40ea2ae0a19
skytunnel <32209413+skytunnel@users.noreply.github.com>
2023-10-24 20:59:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@
 	"author": "Stuart Houston",
 	"authorUrl": "https://new.godresource.com/video",
 	
-	"sourceUrl": "https://raw.githubusercontent.com/skytunnel/GodResourcePluginUnoffical/main/config.js",
+	"sourceUrl": "https://raw.githubusercontent.com/skytunnel/GodResourcePluginUnoffical/dev/config.js",
 	"repositoryUrl": "https://github.com/skytunnel/GodResourcePluginUnoffical",
-	"scriptUrl": "https://raw.githubusercontent.com/skytunnel/GodResourcePluginUnoffical/main/script.js",
+	"scriptUrl": "https://raw.githubusercontent.com/skytunnel/GodResourcePluginUnoffical/dev/script.js",
 	"version": 1,
 	
 	"iconUrl": "https://www.godresource.com/Images/Logos/GRLogo.jpg",
