2a9d0dc5a3d68896c73d7f30f73c17b2b1c05c5b
skytunnel <32209413+skytunnel@users.noreply.github.com>
2023-10-26 20:22:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 {
-	"name": "God Resource (Unofficial)",
-	"description": "Sermons from nifb",
-	"author": "Stuart Houston",
-	"authorUrl": "https://new.godresource.com/video",
-	
-	"sourceUrl": "https://raw.githubusercontent.com/skytunnel/GodResourcePluginUnoffical/dev/config.js",
-	"repositoryUrl": "https://github.com/skytunnel/GodResourcePluginUnoffical",
-	"scriptUrl": "https://raw.githubusercontent.com/skytunnel/GodResourcePluginUnoffical/dev/script2.js",
-	"version": 1,
-	
-	"iconUrl": "https://www.godresource.com/Images/Logos/GRLogo.jpg",
-	"id": "482834c5-bba1-409e-a08b-4fb61d9e4232",
-	
-	"scriptSignature": "<ommitted>",
-	"scriptPublicKey": "<ommitted>",
-	"packages": ["Http"],
-	
-	"allowEval": false,
-	"allowUrls": [
-		"everywhere"
-	]
+    "name": "God Resource (Unofficial)",
+    "description": "Sermons from nifb",
+    "author": "Stuart Houston",
+    "authorUrl": "https://new.godresource.com/video",
+    
+    "repositoryUrl": "https://github.com/skytunnel/GodResourcePluginUnoffical/dev",
+    "sourceUrl": "./config.js",
+    "scriptUrl": "./script.js",
+    "version": 1,
+    
+    "iconUrl": "https://www.godresource.com/Images/Logos/GRLogo.jpg",
+    "id": "482834c5-bba1-409e-a08b-4fb61d9e4232",
+    
+    "scriptSignature": "<ommitted>",
+    "scriptPublicKey": "<ommitted>",
+    "packages": ["Http"],
+    
+    "allowEval": false,
+    "allowUrls": [
+        "everywhere"
+    ]
 }
